36cb62fd618c0b661247338b370944f20366587f
michaelyohannes123 <miketj2019@gmail.com>
2019-07-26 10:56:33 -0700

    r

--- main.py
+++ main.py
@@ -131,7 +131,9 @@ class MoodPage(webapp2.RequestHandler): #get, post request in javascript
 class DailyRecPage(webapp2.RequestHandler): #get, post, keyError
     def get(self):
         #get user location through google maps api and detail the current time and location
-        user=users.get_current_user()
+        dailyrec_template=jinja_env.get_template('templates/dailyrec.html')
+        self.response.write(dailyrec_template.render(vars))
+        '''user=users.get_current_user()
         vars={}
         attr=None
         if user:
@@ -159,7 +161,7 @@ class DailyRecPage(webapp2.RequestHandler): #get, post, keyError
         self.response.write(address)
         self.response.write(" -> displays activity recommendations based on personality quiz")
         vars={date,data,address}
-        self.response.write(dailyrec_template.render(vars))
+        self.response.write(dailyrec_template.render(vars))'''
 #ONE QUESTION COULD BE ABOUT IF YOU PREFER TO TRAVEL FAR
 class FoodHandler(webapp2.RequestHandler):#LINK http://localhost:8080/foodhandler on food tab
     def get(self):
@@ -214,6 +216,7 @@ class SocialPage(webapp2.RequestHandler): #get, post
         attr=None
         if user:
             em=user.nickname()
+            print(em)
             attr=User.query().filter(User.email==em).fetch()
         else:
             self.redirect('/reciever')
